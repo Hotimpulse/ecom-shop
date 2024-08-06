b29e96cdf28ebd5f9280044c3eea807815ebca73
Hotimpulse <hotimpulse@hotmail.com>
2024-08-06 23:17:22 +0300

    fix: fixing footer styling

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,13 +3,15 @@ import footer from "./footer.module.css";
 export default function Footer() {
   return (
     <footer className={footer.wrapper}>
-      <h1 className={footer.footer}>Goods4you</h1>
-      <nav className={footer.navigation}>
-        <ul className={footer.nav_list}>
-          <li>Catalog</li>
-          <li>FAQ</li>
-        </ul>
-      </nav>
+      <div className={footer["wrapper-menu"]}>
+        <h1 className={footer.footer}>Goods4you</h1>
+        <nav className={footer.navigation}>
+          <ul className={footer.nav_list}>
+            <li>Catalog</li>
+            <li>FAQ</li>
+          </ul>
+        </nav>
+      </div>
     </footer>
   );
 }

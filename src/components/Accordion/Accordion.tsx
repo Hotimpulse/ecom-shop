52370f182bb1e6f17f04c00d82d3e94c0adfa335
Hotimpulse <hotimpulse@hotmail.com>
2024-08-09 23:55:15 +0300

    fix: fixing a fragment key error and some styling blunders

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -2,16 +2,17 @@ import { IAccordionItem } from "@src/interfaces/IAccordionItem";
 import { IAccordionProps } from "@src/interfaces/IAccordionProps";
 import accordion from "./accordion.module.scss";
 import AccordionItem from "./AccordionItem";
+import React from "react";
 
 export default function Accordion({ data }: IAccordionProps) {
   return (
     <div className={accordion.accordion}>
       <hr />
       {data.map((item: IAccordionItem) => (
-        <>
+        <React.Fragment key={item.key}>
           <AccordionItem key={item.key} title={item.title} text={item.text} />
-          <hr />
-        </>
+          <hr key={`hr-${item.key}`} />
+        </React.Fragment>
       ))}
     </div>
   );

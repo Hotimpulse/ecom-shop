5672963e3397482d7b85b1fb6a0d31dd1bfcdd1c
Hotimpulse <hotimpulse@hotmail.com>
2024-08-25 12:51:09 +0300

    feat: adding tests for the getAuthToken function

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from 'path';
-
+import path from "path";
+/// <reference types="vitest" />
 
 // https://vitejs.dev/config/
 export default defineConfig({
